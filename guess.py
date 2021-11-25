fd6fc9f581ea783112b5c4bc219f49a836cd5b06
leosunsun <Leo@Leo-Huang.local>
2021-11-25 17:59:16 +0700

    upgrade1

--- guess.py
+++ guess.py
@@ -1,13 +1,16 @@
 #猜数字游戏
 import random
 r = random.randint(1,100)
+count = 0
 while True: 
+	count += 1
 	guess = input ('猜猜这是几？')
 	guess = int (guess)
 	if guess == r :
-		print ('耶！终于猜对了！！')
+		print ('耶!经过', count , '次努力，你终于猜对了！！')
 		break
 	elif guess > r :
 		print ('比答案大')
 	elif guess < r :
 		print ('比答案小')
+	print ('您已经猜了', count, '次')
